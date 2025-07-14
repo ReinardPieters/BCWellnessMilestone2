07377b3b71bcd2009ee079a3eb8f483a31d0a917
Reinard Pieters <reinardpiet@gmail.com>
2025-07-14 13:42:51 +0200

    Moved to the swing UI on NetBeans

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import dao.DBSetup;
-
-public class Main {
-    public static void main(String[] args) {
-        DBSetup.initializeDatabase();
-    }
-}
---
+++ src/view/BCStudentWellness2.java
@@ -0,0 +1,12 @@
+package view;
+
+public class BCStudentWellness2 {
+
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            MainDashboard dashboard = new MainDashboard();
+            dashboard.setVisible(true); 
+        });
+    }
+}
