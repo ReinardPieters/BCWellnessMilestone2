264f56356b8bf82c8de9293173d1dc2406a4c4e7
Reinard Pieters <reinardpiet@gmail.com>
2025-07-17 18:48:33 +0200

    Added all of the comments for Appointments Controller and Counselor Controller as well as the timeUtils file

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import dao.DBSetup;
 //run this to make the database and only run once please
 public class Main {
     public static void main(String[] args) {
+        // Initialize the database
         DBSetup.initializeDatabase();
+        // Print the feedback columns
         DBSetup.printFeedbackColumns();
     }
 }
